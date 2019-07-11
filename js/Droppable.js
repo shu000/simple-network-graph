1b5ecb5206dd7716c1e1f8aad67fe4561c77e388
miyagon <miyagon@example.com>
2019-07-12 01:33:02 +0900

    CSVを読むところまで

--- js/Droppable.js
+++ js/Droppable.js
@@ -1,26 +1,36 @@
 class Droppable {
+
   constructor(area) {
     this.droppableArea = area;
 		this.droppableArea.addEventListener("dragenter", cancelEvent);
 		this.droppableArea.addEventListener("dragover", cancelEvent);
-  };
+  }
 
-  addDropdEvent(callback) {
+  hide() {
+    this.droppableArea.setAttribute("style", "display: none;");
+  }
+
+  on(type, callback) {
+    // drop イベントのみである限りはここでretrun
+    if (type !== "drop") return;
+
 		this.droppableArea.addEventListener("drop", (e) => {
+      this.hide();
+
 			e.preventDefault();
 			// TODO: 複数ファイルドロップ時のエラー処理
 			const file = e.dataTransfer.files[0];
 
-    	//if (file.type !== "application/json") return null;
+    	if (file.type !== "text/csv") return null;
 
     	const reader = new FileReader();
     	reader.onload = e => {
-        console.log(e.target.result);
         callback(e.target.result);
     	};
-    	reader.readAsText(file);
+
+      reader.readAsText(file);
 		});
-  };
+  }
 };
 
 const cancelEvent = (e) => {
