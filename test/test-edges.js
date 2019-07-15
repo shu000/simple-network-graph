7fec64f93ad494f0569f771778ed792085c93087
miyagon <miyagon@example.com>
2019-07-15 22:41:33 +0900

    test: Edges.js #initByKeywords()

--- test/test-edges.js
+++ test/test-edges.js
@@ -1,8 +1,22 @@
 // mocha --require babel-core/register ./test/test-edges.js
 const assert = require("assert");
+const Words = require("../public/js/Words").default;
 const Edges = require("../public/js/Edges").default;
 
 describe("Edges", function() {
+
+  describe("#getAsVisEdges", function(){
+    it("should return Array as inputs to Vis.js's setDataSet()", function() {
+      const edges = new Edges();
+      edges.append(1, 2, 30);
+      assert.deepStrictEqual(edges.getAsVisEdges(), [{
+        id: 0,
+        from: 1,
+        to: 2
+      }]);
+    });
+  });
+
   describe("#append()", function() {
     it("should append args when not exists same edge", function() {
       const edges = new Edges();
@@ -34,6 +48,29 @@ describe("Edges", function() {
         wordIDs: [1, 2],
         coOccurence: 60
       }]);
+    });
+  });
+
+  describe("#initByKeywords()", function() {
+    it("should append edges in accordance with arg", function() {
+      const keywords = [
+        { keyword: "Sunday Monday Tuesday", occurence:  10},
+        { keyword: "Sunday Wednesday", occurence:  20},
+        { keyword: "Monday Tuesday", occurence:  20}
+      ];
+
+      const words = new Words();
+      words.initByKeywords(keywords)
+
+      const edges = new Edges();
+      edges.initByKeywords(keywords, words);
+
+      assert.deepStrictEqual(edges.getAll(), [
+        { edgeID: 0, wordIDs: [ 0, 1 ], coOccurence: 10 },
+        { edgeID: 1, wordIDs: [ 0, 2 ], coOccurence: 10 },
+        { edgeID: 2, wordIDs: [ 1, 2 ], coOccurence: 30 },
+        { edgeID: 3, wordIDs: [ 0, 3 ], coOccurence: 20 },
+      ]);
     });
   });
 });
