518c1964282d90fc5707910319a73c1154c155b2
miyagon <miyagon@example.com>
2019-07-15 17:55:45 +0900

    test: Words.js's new functions.

--- test/test-words.js
+++ test/test-words.js
@@ -1,38 +1,90 @@
-const assert = require('assert');
-
 // mocha --require babel-core/register ./test/test-words.js
+
+const assert = require("assert");
 // TODO: この require したオブジェクトの default プロパティ取るのって正しくない気がする。。
 const Words = require("../public/js/Words").default;
 
-describe('Words', function() {
-  describe('#get()', function() {
-    it('should return [] when empty', function() {
+describe("Words", function() {
+  describe("#getAll()", function() {
+    it("should return [] when empty", function() {
       const words = new Words();
-      assert.deepStrictEqual(words.get(), []);
+      assert.deepStrictEqual(words.getAll(), []);
     });
   });
 
-  describe('#append()', function() {
-    it('should append args when not exists same word', function() {
+  describe("#getID()", function() {
+    it("should return wordID when exists searching word", function() {
       const words = new Words();
       words.append("StarPlutinum", 999);
-      assert.deepStrictEqual(words.get(), [{
+      words.append("TheWorld", 500);
+      assert.strictEqual(words.getID("TheWorld"), 1);
+    });
+
+    it("should return -1 when not exists searching word", function() {
+      const words = new Words();
+      words.append("StarPlutinum", 999);
+      assert.strictEqual(words.getID("TheWorld"), -1);
+    });
+  });
+
+  describe("#append()", function() {
+    it("should append args when not exists same word", function() {
+      const words = new Words();
+      words.append("StarPlutinum", 999);
+      assert.deepStrictEqual(words.getAll(), [{
         wordID: 0,
         text: "StarPlutinum",
         occurence: 999
       }]);
     });
 
-    it('should add args.occurence to words.occurende when exists same word', function() {
+    it("should add args.occurence to words.occurence when exists same word", function() {
       const words = new Words();
-      words.append("The World", 500);
-      words.append("The World", 500);
-      assert.deepStrictEqual(words.get(), [{
+      words.append("TheWorld", 500);
+      words.append("TheWorld", 500);
+      assert.deepStrictEqual(words.getAll(), [{
         wordID: 0,
-        text: "The World",
+        text: "TheWorld",
         occurence: 1000
       }]);
     });
   });
 
+  describe("#initByKeywords()", function() {
+    it("should append words in accordance with arg", function() {
+      const keywords = [
+        { keyword: "Sunday", occurence: 99},
+        { keyword: "Monday Tuesday", occurence:  10},
+        { keyword: "Tuesday Wednesday", occurence:  20}
+      ];
+
+      const words = new Words();
+      words.initByKeywords(keywords);
+      assert.deepStrictEqual(words.getAll(), [
+        { wordID: 0, text: "Sunday", occurence: 99 },
+        { wordID: 1, text: "Monday", occurence: 10 },
+        { wordID: 2, text: "Tuesday", occurence: 30 },
+        { wordID: 3, text: "Wednesday", occurence: 20 }
+      ]);
+    });
+  });
+
+  describe("#getAllOrderByOccurence()", function() {
+    it("should return Array sorted by occurence", function() {
+      const keywords = [
+        { keyword: "Sunday", occurence: 99},
+        { keyword: "Monday Tuesday", occurence:  10},
+        { keyword: "Tuesday Wednesday", occurence:  20}
+      ];
+
+      const words = new Words();
+      words.initByKeywords(keywords);
+      assert.deepStrictEqual(words.getAllOrderByOccurence(), [
+        { wordID: 0, text: "Sunday", occurence: 99 },
+        { wordID: 2, text: "Tuesday", occurence: 30 },
+        { wordID: 3, text: "Wednesday", occurence: 20 },
+        { wordID: 1, text: "Monday", occurence: 10 }
+      ]);
+    });
+  });
 });
