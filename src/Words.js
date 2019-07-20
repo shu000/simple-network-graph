e9713bd995e7e63f52215e1b9ad1c0ec93af5a5c
miyagon <miyagon@example.com>
2019-07-20 22:45:50 +0900

    feat: scale circles's size by rank of occurence

--- src/Words.js
+++ src/Words.js
@@ -28,18 +28,23 @@ class Words {
    * @return {Array.<Object>} input to Vis.js's setDataSet()
    */
   getAsVisNodes() {
-    const nodes = [];
-    this.words.map(word => {
-      nodes.push({
+    const len = this.words.length;
+    return this.getAllOrderByOccurence().map((word, i) => {
+      return {
         id: word.wordID,
         label: word.text,
-        value: word.occurence,
+        value: this.calcVisValueBy(word.occurence),
         scaling: {
           label: { enabled: true }
         }
-      });
+      };
     });
-    return nodes;
+  }
+
+  calcVisValueBy(occurence) {
+    const index = this.uniqueOccurences.findIndex(x => x === occurence);
+    const percentage = ((index + 1) / this.uniqueOccurences.length) * 100;
+    return values.find(x => percentage <= x.percentage).value;
   }
 
   /**
@@ -80,7 +85,23 @@ class Words {
         this.append(text, occurence);
       });
     });
+
+    this.initUniqueOccurences();
+  }
+
+  initUniqueOccurences() {
+    const occurences = this.getAllOrderByOccurence().map(word => word.occurence);
+    this.uniqueOccurences = [...new Set(occurences)];
   }
 }
+
+// if a word.occurence is top 20% of all this.uniqueOccurences, value = 10
+const valueRanks = [
+  { percentage:  20, value: 10 },
+  { percentage:  40, value: 8 },
+  { percentage:  60, value: 6 },
+  { percentage:  80, value: 4 },
+  { percentage: 100, value: 2 },
+];
 
 module.exports = Words;
